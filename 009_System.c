b304f84b6e65fa18decb0c7d03790c8289f34d5c
abc3240660 <damon@damon.com>
2020-01-09 03:38:46 +0800

    New HW IAP OK

--- 009_System.c
+++ 009_System.c
@@ -1,17 +1,16 @@
-//       10        20        30        40        50        60        70
-//--*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+//*****************************************************************************
+// A library for System Configuration of PIC24
+// This file is about the System Configuration
+//
+// Copyright (c) 2019 Mobit technology inc.
+// @Author       : Wim Meganck
+// @Create time  : 03/12/2019
+//*****************************************************************************
+#include "009_System.h"
 
-/******************************************************************************
- * A library for System Configuration of PIC24
- * This file is about the System Configuration
- *
- * Copyright (c) 2019 Mobit technology inc.
- * @Author       : Damon
- * @Create time  : 03/11/2019
-******************************************************************************/
-// Add Support for PIC24FJ1024GA606
-#include "015_Common.h"
-
+//******************************************************************************
+//* System clock configuration
+//******************************************************************************
 // FSEC
 #pragma config BWRP = OFF               // Boot Segment Write-Protect bit (Boot Segment may be written)
 #pragma config BSS = DISABLED           // Boot Segment Code-Protect Level bits (No Protection (other than BWRP))
@@ -25,56 +24,42 @@
 // FBSLIM
 #pragma config BSLIM = 0x1FFF           // Boot Segment Flash Page Address Limit bits (Enter Hexadecimal value)
 
-// FSIGN
-
-#ifdef EXT_OSC_4M_USE
 // FOSCSEL
-#pragma config FNOSC = PRIPLL           // Oscillator Source Selection (Primary Oscillator with PLL module (XT + PLL, HS + PLL, EC + PLL))
-#pragma config PLLMODE = PLL8X          // PLL Mode Selection (8x PLL selected)
+#ifndef OSC_20M_USE
+#pragma config FNOSC = FRC              // Oscillator Source Selection (Oscillator with Frequency Divider)
+#pragma config PLLMODE = PLL96DIV2      // PLL Mode Selection (No PLL used; PLLEN bit is not available)
 #pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+#else
+#pragma config FNOSC = OSCFDIV          // Oscillator Source Selection (Oscillator with Frequency Divider)
+#pragma config PLLMODE = PLL96DIV5      // PLL Mode Selection (No PLL used; PLLEN bit is not available)
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+#endif
 
 // FOSC
-#pragma config POSCMD = XT            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#ifndef OSC_20M_USE
+#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
 #pragma config OSCIOFCN = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
-#pragma config SOSCSEL = ON             // SOSC Power Selection Configuration bits (SOSC is used in crystal (SOSCI/SOSCO) mode)
+#pragma config SOSCSEL = OFF            // SOSC Power Selection Configuration bits (SOSC is used in crystal (SOSCI/SOSCO) mode)
+#pragma config PLLSS = PLL_FRC          // PLL Secondary Selection Configuration bit (PLL is fed by the Primary oscillator)
+#pragma config IOL1WAY = OFF            // Peripheral pin select configuration bit (Allow only one reconfiguration)
+#pragma config FCKSM = CSECMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
+#else
+#pragma config POSCMD = HS              // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#pragma config OSCIOFCN = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
+#pragma config SOSCSEL = OFF            // SOSC Power Selection Configuration bits (SOSC is used in crystal (SOSCI/SOSCO) mode)
 #pragma config PLLSS = PLL_PRI          // PLL Secondary Selection Configuration bit (PLL is fed by the Primary oscillator)
 #pragma config IOL1WAY = ON             // Peripheral pin select configuration bit (Allow only one reconfiguration)
-#pragma config FCKSM = CSDCMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
-#else// 20M External OSC or without External OSC
-// FOSCSEL
-#ifdef EXT_CRYSTAL
-#pragma config FNOSC = PRIPLL    //Oscillator Source Selection->Primary Oscillator (XT, HS, EC)
-#pragma config PLLMODE = PLL96DIV5    //PLL Mode Selection->No PLL used; PLLEN bit is not available
-#else
-#pragma config FNOSC = FRCPLL    //Oscillator Source Selection->Primary Oscillator (XT, HS, EC)
-#pragma config PLLMODE = PLL96DIV2    //PLL Mode Selection->No PLL used; PLLEN bit is not available
-#endif
-
-#pragma config IESO = OFF    //Two-speed Oscillator Start-up Enable bit->Start up with user-selected oscillator source
-
-// FOSC
-#ifdef EXT_CRYSTAL
-#pragma config POSCMD = HS    //Primary Oscillator Mode Select bits->HS Crystal Oscillator Mode
-#else
-#pragma config POSCMD = NONE    //Primary Oscillator Mode Select bits->HS Crystal Oscillator Mode
-#endif
-
-#pragma config OSCIOFCN = ON    //OSC2 Pin Function bit->OSC2 is general purpose digital I/O pin
-#pragma config SOSCSEL = OFF    //SOSC Power Selection Configuration bits->Digital (SCLKI) mode
-#pragma config PLLSS = PLL_PRI    //PLL Secondary Selection Configuration bit->PLL is fed by the Primary oscillator
-#pragma config IOL1WAY = ON    //Peripheral pin select configuration bit->Allow only one reconfiguration
-#pragma config FCKSM = CSECMD    //Clock Switching Mode bits->Clock switching is enabled,Fail-safe Clock Monitor is disabled
+#pragma config FCKSM = CSECMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
 #endif
 
 // FWDT
-// PS2048 + PR128 -> 8.192s
-#pragma config WDTPS = PS2048          // Watchdog Timer Postscaler bits (1:32,768)
+#pragma config WDTPS = PS2048           // Watchdog Timer Postscaler bits (1:32,768)
 #pragma config FWPSA = PR128            // Watchdog Timer Prescaler bit (1:128)
-#pragma config FWDTEN = ON_SWDTEN              // Watchdog Timer Enable bits (WDT Enabled)
+#pragma config FWDTEN = ON_SWDTEN       // Watchdog Timer Enable bits (WDT Enabled)
 #pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
 #pragma config WDTWIN = WIN25           // Watchdog Timer Window Select bits (WDT Window is 25% of WDT period)
 #pragma config WDTCMX = WDTCLK          // WDT MUX Source Select bits (WDT clock source is determined by the WDTCLK Configuration bits)
-#pragma config WDTCLK = LPRC            // WDT Clock Source Select bits (WDT uses LPRC)
+#pragma config WDTCLK = FRC//LPRC            // WDT Clock Source Select bits (WDT uses LPRC)
 
 // FPOR
 #pragma config BOREN = ON               // Brown Out Enable bit (Brown Out Enable Bit)
@@ -82,7 +67,7 @@
 #pragma config DNVPEN = ENABLE          // Downside Voltage Protection Enable bit (Downside protection enabled using ZPBOR when BOR is inactive)
 
 // FICD
-#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
+#pragma config ICS = PGD2               // ICD Communication Channel Select bits (Communicate on PGEC2 and PGED2)
 #pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
 #pragma config BTSWP = OFF              // BOOTSWP Disable (BOOTSWP instruction disabled)
 
@@ -92,16 +77,34 @@
 #pragma config SOSCHP = ON              // SOSC High Power Enable bit (valid only when SOSCSEL = 1 (Enable SOSC high power mode (default))
 #pragma config ALTVREF = ALTREFEN       // Alternate Voltage Reference Location Enable bit (VREF+ and CVREF+ on RA10, VREF- and CVREF- on RA9)
 
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
-#include <xc.h>
-
-#include "009_System.h"
-
+//******************************************************************************
+//* System clock switch to PLL
+//******************************************************************************
 void System_Config(void)
 {
-    CLKDIV = 0;
-    _WDTO = 0;// Clear Flag
-    _SWDTEN = 0;// Disable WDT
+#ifndef OSC_20M_USE
+    //CLKDIV = 0x1000;
+    
+    __builtin_write_OSCCONH(0x01);
+
+    // Start clock switching
+   __builtin_write_OSCCONL(0x01);    
+
+    // Wait for Clock switch to occur (COSC = 0b011)
+    while (OSCCONbits.COSC != 0b001);
+
+    // Wait for PLL to lock
+    while(OSCCONbits.LOCK!=1);
+#else
+    __builtin_write_OSCCONH(0x03);
+
+    // Start clock switching
+    __builtin_write_OSCCONL(0x01);    
+
+    // Wait for Clock switch to occur (COSC = 0b011)
+    while (OSCCONbits.COSC != 0b011);
+
+    // Wait for PLL to lock
+    while(OSCCONbits.LOCK!=1);
+#endif 
 }
